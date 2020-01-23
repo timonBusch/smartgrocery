cab316c622c77ce4b998d44704c777e727e3ce97
timonBusch <timon.buschendorf@gmx.de>
2020-01-23 01:16:42 +0100

    REST API Signup

--- app/src/main/java/com/example/smartgrocery/SignUpActivity.kt
+++ app/src/main/java/com/example/smartgrocery/SignUpActivity.kt
@@ -8,6 +8,9 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.ProgressBar
 import android.widget.Toast
+import com.example.smartgrocery.repositories.SignUpRepository
+import com.example.smartgrocery.repositories.SignUpRepository.body
+import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class SignUpActivity : AppCompatActivity() {
 
@@ -16,6 +19,7 @@ class SignUpActivity : AppCompatActivity() {
     var signupUserNameText: EditText? = null
     var signupPasswordText: EditText? = null
     var signupRePasswordText: EditText? = null
+    var signupEmailText: EditText? = null
 
 
 
@@ -28,6 +32,7 @@ class SignUpActivity : AppCompatActivity() {
         signupPasswordText = findViewById(R.id.signup_password)
         signupRePasswordText = findViewById(R.id.signup_repassword)
         signupUserNameText = findViewById(R.id.signup_name_input)
+        signupEmailText = findViewById(R.id.email_input)
 
         register_Button!!.setOnClickListener{
             progressBar!!.visibility = View.VISIBLE
@@ -49,15 +54,18 @@ class SignUpActivity : AppCompatActivity() {
 
         val userName = signupUserNameText!!.text.toString()
         val password = signupPasswordText!!.text.toString()
-        val rePassword = signupRePasswordText!!.text.toString()
+        val email = signupEmailText!!.text.toString()
 
         val corretlySingedUp = validate()
 
         // TODO: SignUp logic (send information to server)
-
+        SignUpRepository.sendUserData(userName, password, email)
 
         android.os.Handler().postDelayed(
             {
+                val serverResponse = body
+                val corretlySingedUp = serverResponse.equals("saved")
+
                 if(corretlySingedUp) {
                     onSignUpSuccess()
                 } else {
@@ -71,6 +79,8 @@ class SignUpActivity : AppCompatActivity() {
     private fun onSignUpSuccess() {
         progressBar!!.visibility = View.GONE
         register_Button!!.isEnabled = true
+        Toast.makeText(baseContext, "Registration successfull",
+            Toast.LENGTH_LONG).show()
 
         startActivity(Intent(this, LoginActivity::class.java))
         finish()
@@ -88,6 +98,7 @@ class SignUpActivity : AppCompatActivity() {
         val userName = signupUserNameText!!.text.toString()
         val password = signupPasswordText!!.text.toString()
         val rePassword = signupRePasswordText!!.text.toString()
+        val email = signupEmailText!!.text.toString()
 
         // Check if the user name has the right form
         if (userName.isEmpty() || userName.length < 3) {
@@ -96,6 +107,15 @@ class SignUpActivity : AppCompatActivity() {
         } else {
             signupUserNameText!!.error = null
         }
+
+        // Check if email is valid
+        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            email_input!!.error = "enter a valid email address"
+            valid = false
+        } else {
+            email_input!!.error = null
+        }
+
 
         // Check if the password has the right form
         if (password.isEmpty() || password.length < 4 || password.length > 10) {
--- app/src/main/java/com/example/smartgrocery/repositories/SignUpRepository.kt
+++ app/src/main/java/com/example/smartgrocery/repositories/SignUpRepository.kt
@@ -5,17 +5,20 @@ import java.io.IOException
 
 object SignUpRepository {
 
+    var body: String? = null
+
     /**
      * Send user data through REST API to the server
      *
      */
-    fun sendUserData(userName: String, password: String) {           // Parameter User name und password
-        println("Attempting to user data")
+    fun sendUserData(userName: String, password: String, email: String) {           // Parameter User name und password
+        println("Attempting to send user data")
 
         // Rest API URL
         // Make String in dependence of user name and password
         // TODO: Replace URL with new created user information
-        val url = "https://jsonplaceholder.typicode.com/todos/1"
+        //val url = "https://jsonplaceholder.typicode.com/todos/1"
+        val url = "http://192.168.178.20:8080/smart-grocery-0.0.1-SNAPSHOT/username/add/$userName/$password/$email"
 
         val request = Request.Builder().url(url).build()
 
@@ -24,7 +27,7 @@ object SignUpRepository {
         // Make request on REST API
         client.newCall(request).enqueue(object: Callback {
             override fun onResponse(call: Call, response: Response) {
-                val body = response.body()?.string()
+                body = response.body()?.string()
 
                 // TODO: Check response if user was created
 
